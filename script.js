c58b87618a2019a7657872dac455928c87442353
Harsh Lad <91070567+harshlad49@users.noreply.github.com>
2025-06-17 22:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function secondHighest(arr) {
-    if (arr.length < 2) return -Infinity;
 
-    let max = -Infinity;
-    let secondMax = -Infinity;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > max) {
-            secondMax = max;
-            max = arr[i];
-        } else if (arr[i] > secondMax && arr[i] < max) {
-            secondMax = arr[i];
-        }
-    }
-
-    return secondMax === -Infinity ? -Infinity : secondMax;
-}
